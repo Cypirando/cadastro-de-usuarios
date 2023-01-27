2a13de70ff3c3a368b3a0e49b9c5ca20697e6ee6
cypirando <fabiocypriano@gmail.com>
2023-01-27 02:13:06 -0300

    Correção de bugs

--- src/pages/Interesses/index.jsx
+++ src/pages/Interesses/index.jsx
@@ -3,25 +3,31 @@ import { useEffect, useState } from "react";
 import Atributos from "../../components/Atributos/Atributos";
 const Interesses = ({ formik }) => {
   const dataSource = Object.values(Atributos);
-  const [selectedInteresses, setSelectedInteresses] = useState([]);
+  const [selectedInteresses, setSelectedInteresses] = useState(
+    JSON.parse(localStorage.getItem("selectedInteresses")) || []
+  );
 
-
-const handleCheckboxChange = (event) => {
-  const { name, checked } = event.target;
-  if (checked) {
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    if (checked) {
       setSelectedInteresses([...selectedInteresses, name]);
-  } else {
+    } else {
       setSelectedInteresses(selectedInteresses.filter((i) => i !== name));
-  }
-  localStorage.setItem("selectedInteresses", JSON.stringify(selectedInteresses));
-};
+    }
+  };
 
-useEffect(() => {
-  const storedInteresses = localStorage.getItem("selectedInteresses");
-  if (storedInteresses) {
+  useEffect(() => {
+    if (selectedInteresses) {
+      localStorage.setItem(
+        "selectedInteresses",
+        JSON.stringify(selectedInteresses)
+      );
+    }
+    const storedInteresses = localStorage.getItem("selectedInteresses");
+    if (storedInteresses) {
       setSelectedInteresses(JSON.parse(storedInteresses));
-  }
-}, []);
+    }
+  }, [selectedInteresses]);
 
   const columns = [
     {
@@ -36,7 +42,7 @@ useEffect(() => {
       render: (children) => (
         <>
           {children.map((child, index) => (
-            <Checkbox 
+            <Checkbox
               key={index}
               name={child.label}
               onChange={handleCheckboxChange}
